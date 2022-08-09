8be09fd352600d39c8a78e0f5030bd3d54cdf49a
Manuel Rodriguez <victorrguezm@gmail.com>
2022-08-09 22:33:15 +0200

    Refactor del listado de proceso

--- src/listado-proceso/script.js
+++ src/listado-proceso/script.js
@@ -1,12 +1,11 @@
 
 $(document).ready(function()
 {
-    const GET_INCIDENCIAS = "http://localhost/backend/src/Server.php?request=getIncidencia";
     const GET_ALL_PROCESOS = "http://localhost/backend/src/Server.php?request=getAllProcesos";
     const GET_PROCESO_BY_ID = "http://localhost/backend/src/Server.php?request=getProcesoById&id=";
+    const GET_INCIDENCIAS_BY_ID = "http://localhost/backend/src/Server.php?request=getIncidencia&id=";
 
-    var values = httpGetRequest(GET_ALL_PROCESOS);
-    if (values) $("#list").append(crearEntradaLista(values));
+    var values = "";
 
     $("#buscar").on("click", function (e)
     {
@@ -22,12 +21,15 @@ $(document).ready(function()
                 values = httpGetRequest(GET_PROCESO_BY_ID + $("#termino-busqueda").val());
         }
         
-        if ((values["code"] == 200) && (values["data"].length > 0))
-            $("#list").append(crearEntradaLista(values));
-        else
-            displayDisclaimer();
+        if (resultadoValido(values)) crearEntradasLista(values);
+        else displayDisclaimer();
     });
 
+    $("#descarga").on("click", function (e)
+    {
+        console.log(JSON.stringify(values.data));
+    });
+    
     $("#volver").on("click", function (e)
     {
         e.preventDefault();
@@ -44,111 +46,116 @@ $(document).ready(function()
         return JSON.parse(xmlHttp.responseText);
     }
 
-    function crearEntradaLista(values)
+    function crearEntradasLista(values)
     {
-        var result = "";
+        values.data.forEach(element => {
+            var entradaLista = crearHTMLProceso(element["id_personalizado"],
+                                                element["hora_inicio"],
+                                                element["hora_fin"],
+                                                element["jefe"],
+                                                element["linea"], element["producto"],
+                                                element["kilos_reales"], 
+                                                element["kilos_teoricos"]);
 
-        for (var data in values.data)
-        {
-            var idPersonalizado = values.data[data]["id_personalizado"];
-            var horaInicio = values.data[data]["hora_inicio"];
-            var horaFin = values.data[data]["hora_fin"];
-            var jefe = values.data[data]["jefe"];
-            var linea = values.data[data]["linea"];
-            var producto = values.data[data]["producto"];
-            var kilosReales = values.data[data]["kilos_reales"];
-            var kilosTeoricos = values.data[data]["kilos_teoricos"];
-            var eficiencia = calcularEficiencia(kilosReales, kilosTeoricos);
+            var incidencias = httpGetRequest(GET_INCIDENCIAS_BY_ID + element["id_proceso"]);
+            
+            if (resultadoValido(incidencias))
+            {
+                var indice = 0;
+                var listaIncidencia = $("<div class='lista-proceso'>" +
+                                      "    <div class='row'>" +
+                                      "        <span class='h5'>Incidencias Proceso Produccion</span>" +
+                                      "    </div>" +
+                                      "</div>");
 
-            result += "<div class='entrada-lista text-left my-3 mb-4'>" +
-                      "    <div class='info-proceso'>" +
-                      "        <div class='row'>" +
-                      "            <div class='col'>"+
-                      "                <span class='h5'>ID: " + idPersonalizado + "</span>" +
-                      "            </div>" +
-                      "            <div class='col'>" +
-                      "                <p>Hora Inicio: " + horaInicio + "</p>" +
-                      "                <p>Hora Fin: " + horaFin + "</p>" +
-                      "            </div>" + 
-                      "            <div class='col'>" +
-                      "                <p>Jefe: " + jefe + "</p>" +
-                      "                <p>Linea: " + linea + "</p>" +
-                      "                <p>Producto: " + producto + "</p>" +
-                      "            </div>" + 
-                      "            <div class='col'>" +
-                      "                <p>Kilos Reales: " + kilosReales + "</p>" +
-                      "                <p>Kilos Teoricos: " + kilosTeoricos + "</p>" +
-                      "                <p>Eficiencia: " + eficiencia + "</p>" +
-                      "            <div>" +
-                      "        </div>" +
-                      "    </div>";
+                incidencias.data.forEach(incidencia => {
+                    var entradaIncidencia = crearHTMLIncidenciaProceso(++indice,
+                        incidencia["descripcion"],
+                        incidencia["hora_parada"],
+                        incidencia["hora_reinicio"]);
 
-            result += crearEntradaListaIncidencia(values.data[data]);
-            result += "</div><hr />";
-        }
+                    listaIncidencia.append(entradaIncidencia);
+                });
 
-        return result;
+                entradaLista.append(listaIncidencia);
+            }
+            
+            entradaLista.append("<hr />");
+
+            $("#list").append(entradaLista);
+        });
     }
 
-    function crearEntradaListaIncidencia(values)
+    function crearHTMLProceso(id, horaInicio, horaFin, jefe, linea, producto,
+                              kilosReales, kilosTeoricos, eficiencia)
     {
-        var incidencias =
-            httpGetRequest(GET_INCIDENCIAS + "&id_proceso=" + values["id_proceso"]);
+        return $("<div class='entrada-lista text-left my-3 mb-4'>" +
+                 "    <div class='info-proceso'>" +
+                 "        <div class='row'>" +
+                 "            <div class='col'>"+
+                 "                <span class='h5'>ID: " + id + "</span>" +
+                 "            </div>" +
+                 "            <div class='col'>" +
+                 "                <p>Hora Inicio: " + horaInicio + "</p>" +
+                 "                <p>Hora Fin: " + horaFin + "</p>" +
+                 "            </div>" + 
+                 "            <div class='col'>" +
+                 "                <p>Jefe: " + jefe + "</p>" +
+                 "                <p>Linea: " + linea + "</p>" +
+                 "                <p>Producto: " + producto + "</p>" +
+                 "            </div>" + 
+                 "            <div class='col'>" +
+                 "                <p>Kilos Reales: " + kilosReales + "</p>" +
+                 "                <p>Kilos Teoricos: " + kilosTeoricos + "</p>" +
+                 "                <p>Eficiencia: " + calcularEficiencia(kilosReales, kilosTeoricos) + "</p>" +
+                 "            <div>" +
+                 "        </div>" +
+                 "    </div>" +
+                 "</div>");
+    }
 
-        var index = 0;
-        var result = "";
-
-        if (incidencias.data.length > 0)
-        {
-            result += "<div class='lista-proceso'>" +
-                      "    <div class='row'>" +
-                      "        <span class='h5'>Incidencias Proceso Produccion</span>" +
-                      "    </div>";
-
-            incidencias.data.forEach(element => {
-
-                var horaParada = element["hora_parada"];
-                var horaReinicio = element["hora_reinicio"];
-
-                result += "<div class='entrada-lista-proceso'>" +
-                        "    <div class='row my-3'>" +
-                        "        <span class='col-1'></span>" +
-                        "        <div class='col-2'>" +
-                        "            <span>" + (++index) + "</span>" +
-                        "        </div>" +
-                        "        <div class='col-9'>" +
-                        "            <p>Descripcion: " + element["descripcion"] + " </p>" +
-                        "        </div>" +
-                        "    </div>" +
-                        "    <div class='row my-3'>" +
-                        "        <span class='col-3'></span>" +
-                        "        <div class='col-3'>" +
-                        "            <p>Hora Parada: " + horaParada + "</p>" +
-                        "        </div>" +
-                        "        <div class='col-3'>" +
-                        "            <p>Hora Reinicio: " + horaReinicio + "</p>" +
-                        "        </div>" +
-                        "        <div class='col-3'>" +
-                        "            <p>Minutos Perdidos: " +
-                                         calcularMinutosPerdidos(horaParada, horaReinicio) +
-                        "            </p>" +
-                        "        </div>" +
-                        "    </div>" +
-                        "</div>";
-            });
-        }
-        
-        return result + "</div>";
+    function crearHTMLIncidenciaProceso(index, descripcion,
+                                        horaParada, horaReinicio)
+    {
+        return $("<div class='entrada-lista-proceso'>" +
+                 "    <div class='row'>" +
+                 "        <span class='col-1'></span>" +
+                 "        <div class='col-2'>" +
+                 "            <span>" + index + "</span>" +
+                 "        </div>" +
+                 "        <div class='col-9'>" +
+                 "            <p>Descripcion: " + descripcion + " </p>" +
+                 "        </div>" +
+                 "    </div>" +
+                 "    <div class='row'>" +
+                 "        <span class='col-3'></span>" +
+                 "        <div class='col-3'>" +
+                 "            <p>Hora Parada: " + horaParada + "</p>" +
+                 "        </div>" +
+                 "        <div class='col-3'>" +
+                 "            <p>Hora Reinicio: " + horaReinicio + "</p>" +
+                 "        </div>" +
+                 "        <div class='col-3'>" +
+                 "            <p>Minutos Perdidos: " +
+                                     calcularMinutosPerdidos(horaParada, horaReinicio) +
+                 "            </p>" +
+                 "        </div>" +
+                 "    </div>" +
+                 "</div>");
     }
 
     function displayDisclaimer()
     {
         var disclaimerCss = "d-flex align-items-center justify-content-center";
 
-        $("#list").append(
-            $("<div id='disclaimer' class='" + disclaimerCss + "'>" +
-                "<h5>No se ha encontrado ningúna entrada con ese término</h5>" +
-            "</div>"));
+        $("#list").append($("<div id='disclaimer' class='" + disclaimerCss + "'>" +
+                            "   <h5>No se ha encontrado ningúna entrada con ese término</h5>" +
+                            "</div>"));
+    }
+
+    function resultadoValido(resultado)
+    {
+        return (resultado["code"] == 200) && (resultado["data"].length > 0);
     }
 
     function vaciarLista()
